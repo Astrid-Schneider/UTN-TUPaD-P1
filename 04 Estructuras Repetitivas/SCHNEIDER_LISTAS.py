9bf13d701967b18451cc9b2cfbfcfcdf83a134c8
Astrid-Schneider <astridelizabethschneider@gmail.com>
2025-05-06 08:44:23 -0300

    Subiendo ejercicios de listas

---
+++ 04 Estructuras Repetitivas/SCHNEIDER_LISTAS.py
@@ -0,0 +1,61 @@
+#Ejercicio 1
+#multiplos_de_4 = list(range(4,101,4))
+#print(multiplos_de_4)
+
+#Ejercicio 2
+#elementos=["gato", "perro", "paloma", "pez", "lechuza"]
+#print(elementos[-2])
+
+#Ejercicio 3
+#lista_vacia=[]
+#lista_vacia.append("gato")
+#lista_vacia.append("perro")
+#lista_vacia.append("paloma")
+#print(lista_vacia)
+
+#Ejercicio 4
+#animales = ["perro", "gato", "conejo", "pez"]
+#animales[1]= "loro"
+#animales[-1]="oso"
+#print(animales)
+
+#Ejercicio 5
+#numeros = [8,15,3,22,7]
+#numeros.remove(max(numeros))
+#print(numeros)
+# El programa elimina el numero mas grande de la lista y luego imprime la lista original con los elementos restantes.
+
+#Ejercicio 6
+#numeros = list(range(10,31,5))
+#print(numeros)
+#primeros_dos = numeros[:2]
+#print(primeros_dos)
+
+#Ejercicio 7
+#autos = ["sedan", "polo", "suran", "gol"]
+#print (autos)
+#autos[1]= "3 CV"
+#autos[2]= "UP"
+#print (autos)
+
+#Ejercicio 8
+#dobles = []
+#dobles.append(5*2)
+#dobles.append(10*2)
+#dobles.append(15*2)
+#print(dobles)
+
+#Ejercicio 9
+#compras = [["pan", "leche"], ["arroz", "fideos", "salsa"],
+#["agua"]]
+#a)
+#compras[2].append("jugo")
+#b)
+#compras[1][1] = "tallarines"
+#c)
+#compras[0].remove("pan")
+#print(compras)
+
+#Ejercicio 10
+#lista_anidada=[15, True, [25.5,57.9,30.6],False]
+#print(lista_anidada)
